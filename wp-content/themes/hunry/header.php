a8450e8384b2eb5002fea5872128ce655edfc337
Yikeng Liang <yikengsr.liang@f1006-19-34183.bmcc.cuny.edu>
2015-10-15 20:59:15 -0400

    first commit

---
+++ wp-content/themes/hunry/header.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>First Theme</title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+<div id="wrapper">
+<div id="header">
+<h1>My First Theme</h1>
+</div>
